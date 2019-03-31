b5db80453444d4d54249a589094ddb08d38d428b
Nero <nerochen25@berkeley.edu>
2019-03-30 18:01:23 -0700

    Add logo and favicon

--- rainbow_pen.js
+++ rainbow_pen.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - 100;
+canvas.height = window.innerHeight - 150;
 ctx.strokeStyle = "#BADASS";
 // ctx.lineJoin = "round";
 // ctx.lineCap = "round";
