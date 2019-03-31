51b2dd122fa153ef95f21f243502539cad31a14a
Nero <nerochen25@berkeley.edu>
2019-03-30 18:05:46 -0700

    Modify input

--- rainbow_pen.js
+++ rainbow_pen.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - 150;
+canvas.height = window.innerHeight - 180;
 ctx.strokeStyle = "#BADASS";
 // ctx.lineJoin = "round";
 // ctx.lineCap = "round";
